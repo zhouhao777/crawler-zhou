6f3a8e8c3f016fee374810cc96f370fddc00f96c
zhouhao <zhouhao@mafengwo.com>
2017-03-27 16:30:49 +0800

    taogu_fix

--- taogu/mostpositive.py
+++ taogu/mostpositive.py
@@ -3,19 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import time
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import json
+import re
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
 
 def onetime():
     timenow = int(time.time())
     parm = str(timenow) + str(random.randint(100,999))
     # print parm
     url = 'https://www.taoguba.com.cn/getNrntActualMatch?_='+ parm
+    # url = 'https://www.taoguba.com.cn/getNrntActualMatch?_=1490583133892'
     print url
-    # 1490547079568
-    # 1490547111327
-
 
     # headers = [
     # {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},
@@ -26,24 +27,40 @@ def onetime():
     # {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'},
     # {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36'}
     # ]
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36',
-               'Accept':'application/json, text/javascript, */*; q=0.01',
-               'Accept-Encoding':'gzip, deflate, sdch, br',
-               'Accept-Language':'zh-CN,zh;q=0.8',
-               'Connection':'keep-alive',
-               'Host':'www.taoguba.com.cn'
+    headers = {
+               'Cookie':'JSESSIONID=f1c29c72-8243-4c7d-bc23-dd6c97baa507; UM_distinctid=15b0d95ad6733e-00b37316d7bbfb-1c3b6b51-1fa400-15b0d95ad68930; CNZZDATA1574657=cnzz_eid%3D1105896313-1490579697-%26ntime%3D1490579697'
                }
     source_code = requests.get(url,headers)
-    plain_text = source_code.text
-    file_content = ''
-    result = 'onetime.txt'
-    soup = BeautifulSoup(plain_text,'html.parser')
-    lines = soup.find_all('li',{'class':'mu103'})
-    print lines
-    for line in lines:
-        for label in line.find_all('label'):
-            name = label.string
-            print name
+    data = json.loads(source_code.text)
+    print data['_t']
+    print data['status']
+    result = []
+    for item in data['dto']['listTopic']:
+        res = re.findall(r"\"(.+?)\"",item['recomStock'])
+        result.append(res)
+    print result
+
+
+def format_data(data):
+    #["sz002683"]_[268336]
+    #["sz002302","sh600425","sz000877"]_[268335,268333,268334]
+    print re.findall(r"\"(.+?)\"",data)#
+
+# format_data('["sz002302","sh600425","sz000877"]_[268335,268333,268334]')
+
+
+
+
+    # print plain_text
+    # file_content = ''
+    # result = 'onetime.txt'
+    # soup = BeautifulSoup(plain_text,'html.parser')
+    # lines = soup.find_all('li',{'class':'mu103'})
+    # print lines
+    # for line in lines:
+    #     for label in line.find_all('label'):
+    #         name = label.string
+    #         print name
     #         if code not in file_content:
     #             file_content += "%s\t%s\n"%(code,name)
     # f = open(result,'w')
